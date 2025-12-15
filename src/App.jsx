de454796aefab3c902194cc9a84cab71bbc17412
etranger2268 <shun.5.2268@gmail.com>
2025-12-15 15:56:45 +0900

    pokemon-3-12

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useEffect } from "react";
-import { getAllPokemon } from "./utils/pokemon";
+import { Fragment, useEffect, useState } from 'react';
+import { getAllPokemon } from './utils/pokemon';
 
 export default function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon/';
 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemon = async () => {
       const res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemon();
-  },[]);
-  return <h1 className="text-4xl text-red-700">App</h1>;
+  }, []);
+
+  return (
+    <Fragment>{loading ? <h1>ロード中</h1> : <h1>ポケモンデータを取得しました</h1>};</Fragment>
+  );
 }
