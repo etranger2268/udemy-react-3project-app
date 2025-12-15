48ce8caff2341dbd396a33654d7657ab3c53514f
etranger2268 <shun.5.2268@gmail.com>
2025-12-15 15:44:58 +0900

    pokemon-3-11

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import { getAllPokemon } from "./utils/pokemon";
+
 export default function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon/';
+
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemon();
+  },[]);
   return <h1 className="text-4xl text-red-700">App</h1>;
 }
