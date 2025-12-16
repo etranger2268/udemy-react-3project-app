5ba76a498ea4d1896982b2a98b32a2d209cba513
etranger2268 <shun.5.2268@gmail.com>
2025-12-16 14:13:38 +0900

    note-5-53

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,20 @@
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <div>
+        <h1>ノート</h1>
+        <button type="button">追加</button>
+      </div>
+      <div>
+        <div>
+          <div>
+            <strong>タイトル</strong>
+            <button type="button">削除</button>
+          </div>
+          <p>ノートの内容です。</p>
+          <small>最後の修正日:xx/xx</small>
+        </div>
+      </div>
+    </div>
+  );
 }
