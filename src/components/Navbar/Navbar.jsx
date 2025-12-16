fc19cb28da16112dcb9dfabb5225f09948a12a52
etranger2268 <shun.5.2268@gmail.com>
2025-12-16 13:59:08 +0900

    preference

--- src/components/Navbar/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="bg-orange-300 text-white h-16 font-semibold text-2xl flex justify-center items-center">
-      ポケモン図鑑
-    </nav>
-  );
-}
