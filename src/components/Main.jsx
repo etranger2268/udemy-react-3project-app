768d9a2d0e0352055d6e0d8ebf1799b014be3549
etranger2268 <shun.5.2268@gmail.com>
2025-12-16 16:25:01 +0900

    note-5-60

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,14 @@
 export default function Main() {
-  return <div>Main</div>
+  return (
+    <div>
+      <div>
+        <input type="text" />
+        <textarea id="" placeholder="ノート内容を記入"></textarea>
+        <div>
+          <h1>タイトル</h1>
+          <div>ノート内容</div>
+        </div>
+      </div>
+    </div>
+  );
 }
