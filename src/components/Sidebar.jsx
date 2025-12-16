f1712be58874ca2f9e9be6ec122b4d1c221d26b0
etranger2268 <shun.5.2268@gmail.com>
2025-12-16 14:31:25 +0900

    note-5-54

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,28 @@
 export default function Sidebar() {
   return (
-    <div>
-      <div>
-        <h1>ノート</h1>
-        <button type="button">追加</button>
+    <div className="w-3/10 h-screen border-r border-gray-200">
+      <div className="flex justify-between p-6">
+        <h1 className="m-0 font-black text-3xl">ノート</h1>
+        <button
+          type="button"
+          className="border-none bg-none text-blue-400 m-0 p-0 text-base cursor-pointer"
+        >
+          追加
+        </button>
       </div>
-      <div>
-        <div>
-          <div>
+      <div className="h-[calc(100vh-78px)] overflow-scroll">
+        <div className="p-6 cursor-pointer transition-all hover:bg-gray-100">
+          <div className="flex justify-between">
             <strong>タイトル</strong>
-            <button type="button">削除</button>
+            <button
+              type="button"
+              className="border-none bg-none text-blue-400 m-0 p-0 text-base cursor-pointer"
+            >
+              削除
+            </button>
           </div>
-          <p>ノートの内容です。</p>
-          <small>最後の修正日:xx/xx</small>
+          <p className="my-2.5 mx-0">ノートの内容です。</p>
+          <small className="text-gray-500">最後の修正日:xx/xx</small>
         </div>
       </div>
     </div>
