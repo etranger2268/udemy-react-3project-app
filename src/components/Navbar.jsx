8e1608468e2eabf006f955235097efc3c11f7b45
etranger2268 <shun.5.2268@gmail.com>
2025-12-22 01:05:23 +0900

    blog-4-30

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,16 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link to="/">ホーム</Link>
-      <Link to="/create-post">記事投稿</Link>
-      <Link to="/login">ログイン</Link>
+    <nav className="flex justify-center items-center h-12 bg-amber-100 gap-11">
+      <Link to="/" className="hover:text-blue-500 transition-all duration-300">
+        ホーム
+      </Link>
+      <Link to="/create-post" className="hover:text-blue-500 transition-all duration-300">
+        記事投稿
+      </Link>
+      <Link to="/login" className="hover:text-blue-500 transition-all duration-300">
+        ログイン
+      </Link>
     </nav>
   );
 }
