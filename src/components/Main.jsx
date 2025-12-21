90f4a8d75a113f43fe01353121dc859ca96b6a52
etranger2268 <shun.5.2268@gmail.com>
2025-12-21 16:56:14 +0900

    note-5-61

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,19 @@
 export default function Main() {
   return (
-    <div>
-      <div>
-        <input type="text" />
-        <textarea id="" placeholder="ノート内容を記入"></textarea>
-        <div>
-          <h1>タイトル</h1>
-          <div>ノート内容</div>
+    <div className="w-7/10 h-screen">
+      <div className="h-[50vh] p-6">
+        <input
+          type="text"
+          className="block border border-gray-300 mb-5 w-full p-1.5 resize-none text-xl h-12"
+        />
+        <textarea
+          id=""
+          placeholder="ノート内容を記入"
+          className="block border border-gray-300 mb-5 w-full h-[calc(50vh-130px)] p-1.5 resize-none text-base"
+        />
+        <div className="h-[50vh] overflow-y-scroll bg-gray-50">
+          <h2 className="font-black text-2xl pt-6 px-6 m-0">タイトル</h2>
+          <div className="leading-8 pb-6 px-6">ノート内容</div>
         </div>
       </div>
     </div>
