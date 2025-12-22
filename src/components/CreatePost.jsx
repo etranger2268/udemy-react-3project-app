1ee7adae3948c0013de5c053453373638ec8ac6c
etranger2268 <shun.5.2268@gmail.com>
2025-12-22 15:36:12 +0900

    blog-4-38

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,4 +1,17 @@
+import { useState } from 'react';
+
 export default function CreatePost() {
+  const initialPost = { title: '', content: '' };
+  const [post, setPost] = useState(initialPost);
+
+  const handleChangePost = (key, value) =>
+    setPost((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+
+  const handleCreatePost = () => console.log(post);
+
   return (
     <div className="flex justify-center items-center h-[90vh]">
       <div className="bg-white flex flex-col justify-center h-100 w-100 gap-2.5 py-0 px-5 shadow-xl ">
@@ -10,22 +23,27 @@ export default function CreatePost() {
           <input
             type="text"
             id="title"
+            value={post.title}
+            onChange={(e) => handleChangePost(e.target.id, e.target.value)}
             placeholder="タイトルを記入"
             className="w-full border border-gray-300 px-1 h-7.5 outline-none"
           />
         </div>
         <div>
-          <label htmlFor="post" className="block">
+          <label htmlFor="content" className="block">
             投稿
           </label>
           <textarea
-            id="post"
+            id="content"
+            value={post.content}
+            onChange={(e) => handleChangePost(e.target.id, e.target.value)}
             placeholder="投稿内容を記入"
             className="w-full border border-gray-300 h-30 outline-none p-1.5"
           />
         </div>
         <button
           type="button"
+          onClick={handleCreatePost}
           className="p-2.5 bg-blue-200 shadow-xl text-white rounded-md cursor-pointer transition-all duration-300 hover:translate-y-1 hover:shadow-none"
         >
           投稿する
