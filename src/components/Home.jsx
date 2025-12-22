0cd54eca96f5ae287f533651ed3e1918b6ac1026
etranger2268 <shun.5.2268@gmail.com>
2025-12-22 16:22:49 +0900

    blog-4-43

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,16 @@
+import { collection, getDocs } from 'firebase/firestore';
+import { useEffect } from 'react';
+import { db } from '../firebase/firebase';
+
 export default function Home() {
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, 'posts'));
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="w-full h-auto flex items-center flex-col p-5">
       <div className="w-150 h-auto max-h-150 bg-white shadow-xl m-5 p-5 rounded-lg space-y-3">
