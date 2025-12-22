6a06eb364cdff0064af0001aec9315198375d274
etranger2268 <shun.5.2268@gmail.com>
2025-12-22 15:19:30 +0900

    blog-4-36

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,3 +1,18 @@
 export default function CreatePost() {
-  return <h1>CreatePost</h1>;
+  return (
+    <div>
+      <div>
+        <h1>記事を投稿する</h1>
+        <div>
+          <label htmlFor="title">タイトル</label>
+          <input type="text" id="title" placeholder="タイトルを記入" />
+        </div>
+        <div>
+          <label htmlFor="post">投稿</label>
+          <textarea id="post" placeholder="投稿内容を記入" />
+        </div>
+        <button type="button">投稿する</button>
+      </div>
+    </div>
+  );
 }
