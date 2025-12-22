7bdfad04b783e9ef0a51e0d192361359668ab3f3
etranger2268 <shun.5.2268@gmail.com>
2025-12-22 16:01:21 +0900

    blog-4-41

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>タイトル</h1>
+        </div>
+      </div>
+      <div>
+        今はReactの学習中です。これから頑張ってReactエンジニアとして活躍していきたいと思っています。よろしくお願いします。
+      </div>
+      <div>
+        <h3>@etranger2268</h3>
+        <button type="button">削除</button>
+      </div>
+    </div>
+  );
 }
